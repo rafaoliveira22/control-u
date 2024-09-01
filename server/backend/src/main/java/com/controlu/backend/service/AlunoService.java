c492dcd6c6b67b41317d0eb8c71f1aab77bdb593
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2024-08-31 21:41:37 -0300

    backend - remoção do AlunoId, RA como unico ID (faltou subir no ultimo commit sobre a alteracao no Aluno)

--- server/backend/src/main/java/com/controlu/backend/service/AlunoService.java
+++ server/backend/src/main/java/com/controlu/backend/service/AlunoService.java
@@ -2,11 +2,10 @@ package com.controlu.backend.service;
 
 import com.controlu.backend.controller.AlunoController;
 import com.controlu.backend.entity.Aluno;
-import com.controlu.backend.entity.embeddable.AlunoId;
 import com.controlu.backend.exception.ResourceNotFoundException;
 import com.controlu.backend.mapper.DozerMapper;
 import com.controlu.backend.repository.AlunoRepository;
-import com.controlu.backend.vo.aluno.AlunoVO;
+import com.controlu.backend.vo.AlunoVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -22,42 +21,25 @@ public class AlunoService {
 
     /**
      * MÉTODO PARA OBTER OS DADOS DE UM ALUNO ESPECÍFICO NA BASE DE DADOS
-     * @param nome NOME DO ALUNO
-     * @param cursoId ID DO CURSO
-     * @param anoIngressao ANO DE INGRESSÃO DO ALUNO NA FACULDADE
+     * @param ra REGISTRO DO ALUNO
      * @return DADOS DO ALUNO ENCONTRADO
      */
-    public AlunoVO obterDadosAluno(String nome, String cursoId, String anoIngressao){
-        AlunoId id = new AlunoId(nome, Integer.parseInt(cursoId), Integer.parseInt(anoIngressao));
-
-        var aluno = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para esse IO."));
-        var vo = DozerMapper.parseObject(aluno, AlunoVO.class);
-        vo.add(linkTo(methodOn(AlunoController.class).obterDadosAluno(vo.getId().getAlunoNome(), String.valueOf(vo.getId().getCursoId()), String.valueOf(vo.getId().getAlunoAnoIngressao()))).withSelfRel());
-
-        return vo;
-    }
-
-    /**
-     * MÉTODO PARA OBTER OS DADOS DE UM ALUNO ESPECÍFICO
-     * @param ra REGISTRO DO ALUNO
-     * @return DADOS DO ALUNO
-     */
     public AlunoVO obterDadosAluno(String ra){
-        var aluno = repository.findByAlunoRa(ra).orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para esse RA."));
+        var aluno = repository.findById(ra).orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para esse ID."));
         var vo = DozerMapper.parseObject(aluno, AlunoVO.class);
-        vo.add(linkTo(methodOn(AlunoController.class).obterDadosAluno(ra)).withSelfRel());
+        vo.add(linkTo(methodOn(AlunoController.class).obterDadosAluno(vo.getAlunoRa())).withSelfRel());
 
         return vo;
     }
 
     /**
-     * MÉTODO PARA OBTER DADOS DE TODOS OS ALUNOS, ORDERNADO POR ANO (DESC) E NOME (ASC)
+     * MÉTODO PARA OBTER DADOS DE TODOS OS ALUNOS, ORDERNADO POR NOME (ASC)
      * @return LISTA COM DADOS DE TOOS OS ALUNOS
      */
 
     public List<AlunoVO> obterDadosDeTodosAlunos(){
-        var alunos = DozerMapper.parseListObjects(repository.findAllByOrderByIdAlunoAnoIngressaoDescIdAlunoNomeAsc(), AlunoVO.class);
-        alunos.stream().forEach(aluno -> aluno.add(linkTo(methodOn(AlunoController.class).obterDadosAluno(aluno.getId().getAlunoNome(), String.valueOf(aluno.getId().getCursoId()), String.valueOf(aluno.getId().getAlunoAnoIngressao()))).withSelfRel()));
+        var alunos = DozerMapper.parseListObjects(repository.findAllByOrderByAlunoNomeAsc(), AlunoVO.class);
+        alunos.stream().forEach(aluno -> aluno.add(linkTo(methodOn(AlunoController.class).obterDadosAluno(aluno.getAlunoRa())).withSelfRel()));
 
         return alunos;
     }
@@ -68,15 +50,13 @@ public class AlunoService {
      * @return ALUNO REGISTRADO
      */
     public AlunoVO registrarDadosAluno(AlunoVO alunoVO){
-        AlunoId id = new AlunoId(alunoVO.getId().getAlunoNome(), alunoVO.getId().getCursoId(), alunoVO.getId().getAlunoAnoIngressao());
-
-        if (repository.existsById(id)) {
+        if (repository.existsById(alunoVO.getAlunoRa())) {
             throw new IllegalArgumentException("Registro já existe.");
         }
 
         Aluno aluno = DozerMapper.parseObject(alunoVO, Aluno.class);
         var vo = DozerMapper.parseObject(repository.save(aluno), AlunoVO.class);
-        vo.add(linkTo(methodOn(AlunoController.class).obterDadosAluno(vo.getId().getAlunoNome(), String.valueOf(vo.getId().getCursoId()),  String.valueOf(vo.getId().getAlunoAnoIngressao()))).withSelfRel());
+        vo.add(linkTo(methodOn(AlunoController.class).obterDadosAluno(vo.getAlunoRa())).withSelfRel());
 
         return vo;
     }
@@ -87,13 +67,12 @@ public class AlunoService {
      * @return ALUNO ATUALIZADO
      */
     public AlunoVO atualizarDadosAluno(AlunoVO alunoVO){
-        AlunoId id = new AlunoId(alunoVO.getId().getAlunoNome(), alunoVO.getId().getCursoId(), alunoVO.getId().getAlunoAnoIngressao());
-
-        var aluno = repository.findByAlunoRa(alunoVO.getAlunoRa()).orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para esse RA."));
-        aluno.getId().setAlunoNome((alunoVO.getId().getAlunoNome()));
+        var aluno = repository.findById(alunoVO.getAlunoRa()).orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para esse RA."));
+        aluno.setAlunoNome(alunoVO.getAlunoNome());
+        aluno.setCursoId(alunoVO.getCursoId());
 
         var vo = DozerMapper.parseObject(repository.save(aluno), AlunoVO.class);
-        vo.add(linkTo(methodOn(AlunoController.class).obterDadosAluno(vo.getId().getAlunoNome(),  String.valueOf(vo.getId().getCursoId()),  String.valueOf(vo.getId().getAlunoAnoIngressao()))).withSelfRel());
+        vo.add(linkTo(methodOn(AlunoController.class).obterDadosAluno(vo.getAlunoRa())).withSelfRel());
 
         return vo;
     }
@@ -103,7 +82,7 @@ public class AlunoService {
      * @param ra REGISTRO DO ALUNO
      */
     public void apagarDadosAlunos(String ra){
-        var aluno = repository.findByAlunoRa(ra).orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para esse RA."));
+        var aluno = repository.findById(ra).orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para esse RA."));
         repository.delete(aluno);
     }
 }
---
+++ server/backend/src/main/resources/db/migration/V13__Drop_Table_Aluno_And_Create_Table_Aluno.sql
@@ -0,0 +1,2 @@
+DROP TABLE aluno;
+CREATE TABLE aluno (aluno_ra VARCHAR(13) PRIMARY KEY NOT NULL, aluno_nome VARCHAR(40) NOT NULL, curso_id int NOT NULL);
