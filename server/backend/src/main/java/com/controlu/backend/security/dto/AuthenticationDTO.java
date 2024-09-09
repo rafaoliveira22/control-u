cf8aaced5e5547a28cc104b7484e622fc6c10864
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2024-09-09 10:59:32 -0300

    backend - ajuste no SecurityConfig para ajustar a permisao do verificarSeEstaRegistrado e adição do host mobile no CORS

--- server/backend/src/main/java/com/controlu/backend/security/dto/AuthenticationDTO.java
+++ server/backend/src/main/java/com/controlu/backend/security/dto/AuthenticationDTO.java
@@ -1,4 +1,11 @@
 package com.controlu.backend.security.dto;
 
 public record AuthenticationDTO(String usuarioNome, String usuarioSenha) {
+    @Override
+    public String toString() {
+        return "AuthenticationDTO{" +
+                "usuarioNome='" + usuarioNome + '\'' +
+                ", usuarioSenha='" + usuarioSenha + '\'' +
+                '}';
+    }
 }
