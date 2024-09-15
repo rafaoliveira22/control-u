a77c51f3e4b28b00f02ac9c65f9185a3c94f224b
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2024-09-15 15:26:33 -0300

    backend - query para buscar grade pelo cartaoId

--- server/backend/src/main/java/com/controlu/backend/repository/GradeRepository.java
+++ server/backend/src/main/java/com/controlu/backend/repository/GradeRepository.java
@@ -4,7 +4,11 @@ import com.controlu.backend.entity.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GradeRepository extends JpaRepository<Grade, Integer> {
     long countByCartaoId(String cartaoId);
+
+    Optional<Grade> findByCartaoId(String cartaoId);
 }
