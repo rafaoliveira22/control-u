215019fcd767cf77c8bc65dad50e443ea445bdb5
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2024-09-15 00:15:53 -0300

    backend - adição da annotation @Repository

--- server/backend/src/main/java/com/controlu/backend/repository/AcessoRepository.java
+++ server/backend/src/main/java/com/controlu/backend/repository/AcessoRepository.java
@@ -3,10 +3,12 @@ package com.controlu.backend.repository;
 import com.controlu.backend.entity.Acesso;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AcessoRepository extends JpaRepository<Acesso, Integer> {
 
     @Query("SELECT a FROM Acesso a WHERE a.alunoId = :alunoId AND DATE(a.acessoEntrada) = CURRENT_DATE")
